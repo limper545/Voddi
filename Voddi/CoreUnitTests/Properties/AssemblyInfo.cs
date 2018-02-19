82c4207e12be3f7c1a63b983744e9cb9bbe281fc
Unknown <limper545@live.com>
2018-02-19 14:16:13 +0100

    Autotests für Validations

---
+++ Voddi/CoreUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoreUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("CoreUnitTests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e40e0b48-2444-4fe5-8dd6-91d6acb32f1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Voddi/CoreUnitTests/UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+
+namespace CoreUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public String vorname, nachname, email, user, passwordOne, passwordTwo;
+        [TestInitialize]
+        public void Initialize()
+        {
+            vorname = "Sascha";
+            nachname = "Test";
+            email = "test@test.de";
+            user = "a";
+            passwordOne = "a";
+            passwordTwo = "a";
+        }
+        [TestMethod]
+        public void Check_If_String_Is_Empty_Or_Not()
+        {
+            String result = Validations.CheckRegistrationValidation(vorname, nachname, email, user, passwordOne, passwordTwo);
+            Assert.AreEqual(result, "OK");
+        }
+
+        [TestMethod]
+        public void Check_If_Password_Is_Empty()
+        {
+            String result;
+            result = Validations.CheckRegistrationValidation(vorname, nachname, email, user, passwordOne, "");
+            Assert.AreEqual(result, "Passwort");
+
+            result = Validations.CheckRegistrationValidation(vorname, nachname, email, user, "", "");
+            Assert.AreEqual(result, "Passwort");
+
+            result = Validations.CheckRegistrationValidation(vorname, nachname, email, user, "", passwordTwo);
+            Assert.AreEqual(result, "Passwort");
+        }
+
+        [TestMethod]
+        public void Check_If_Username_Is_Empty()
+        {
+            String result = Validations.CheckRegistrationValidation(vorname, nachname, email, "", passwordOne, passwordTwo);
+            Assert.AreEqual(result, "Username");
+        }
+
+        [TestMethod]
+        public void Check_If_Email_Is_Empty()
+        {
+            String result = Validations.CheckRegistrationValidation(vorname, nachname, "", user, passwordOne, passwordTwo);
+            Assert.AreEqual(result, "E-Mail Adresse");
+        }
+
+        [TestMethod]
+        public void Check_If_Nachname_Is_Empty()
+        {
+            String result = Validations.CheckRegistrationValidation(vorname, "", email, user, passwordOne, passwordTwo);
+            Assert.AreEqual(result, "Nachname");
+        }
+
+        [TestMethod]
+        public void Check_If_Vorname_Is_Empty()
+        {
+            String result = Validations.CheckRegistrationValidation("", nachname, email, user, passwordOne, passwordTwo);
+            Assert.AreEqual(result, "Vorname");
+        }
+
+        [TestMethod]
+        public void Check_If_Email_Is_Correct_Format()
+        {
+            bool response;
+            response = Validations.CorrectEmailFormat(email);
+            Assert.AreEqual(response, true);
+
+            response = Validations.CorrectEmailFormat("test@test.t");
+            Assert.AreEqual(response, false);
+
+            response = Validations.CorrectEmailFormat("test@test.");
+            Assert.AreEqual(response, false);
+
+            response = Validations.CorrectEmailFormat("test@test");
+            Assert.AreEqual(response, false);
+
+            response = Validations.CorrectEmailFormat("test@");
+            Assert.AreEqual(response, false);
+
+            response = Validations.CorrectEmailFormat("tes");
+            Assert.AreEqual(response, false);
+        }
+    }
+}
